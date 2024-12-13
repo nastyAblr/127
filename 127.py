b626637509031d4ee6813715b2286f14603024b6
Богачева Анастасия <britta18britta@gmail.com>
2024-12-13 16:05:29 +0300

    Add files via upload

---
+++ 127.py
@@ -0,0 +1,13 @@
+from tkinter.scrolledtext import example
+
+example = 'С наступающим Новым годом!'
+print(example[0])
+print(example[-1])
+
+print(example[len(example)//2 :])
+
+print(example[len(example)//2 :][::-1])
+print(example[::2])
+
+
+
